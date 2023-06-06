f95c186e3489df33ae8161f1424e16a25907a1fe
leelanarasimha <leela.narsimha@gmail.com>
2023-06-06 11:44:21 +0530

    setup store

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,17 +1,34 @@
 import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
 
-export const useCounterStore = defineStore('counterStore', {
-  state: () => {
-    return {
-      count: 0
-    };
-  },
-  getters: {
-    doubleCount: (state) => state.count * 2
-  },
-  actions: {
-    increment() {
-      this.count++;
-    }
+// export const useCounterStore = defineStore('counterStore', {
+//   state: () => {
+//     return {
+//       count: 0
+//     };
+//   },
+//   getters: {
+//     doubleCount: (state) => state.count * 2
+//   },
+//   actions: {
+//     increment() {
+//       this.count++;
+//     }
+//   }
+// });
+
+export const useCounterStore = defineStore('counterStore', () => {
+  const count = ref(0);
+
+  const doubleCount = computed(() => count.value * 3);
+
+  function increment() {
+    count.value++;
   }
+
+  return {
+    count,
+    doubleCount,
+    increment
+  };
 });
