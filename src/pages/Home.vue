a7004e2141bd35defd4dd13f65f166d78627a972
leelanarasimha <leela.narsimha@gmail.com>
2023-06-06 21:56:10 +0530

    using the store in component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,29 @@
 <template>
   <div>Home Page</div>
-  <div>Counter: {{ counter.count }}</div>
-  <div>doubleCOunt: {{ counter.doubleCount }}</div>
+  <div>Counter: {{ count }}</div>
+  <div>doubleCOunt: {{ doubleCount }}</div>
   <div>
-    <button @click="counter.increment()">Increment</button>
+    <button @click="increment()">Increment</button>
   </div>
 </template>
 
 <script setup>
+  import { storeToRefs } from 'pinia';
+  import { computed } from 'vue';
   import { useCounterStore } from '../stores/counter';
 
   const props = defineProps(['name']);
   const counter = useCounterStore();
+
+  // const count = counter.count;
+  // const doubleCount = counter.doubleCOunt;
+  // const {count, doubleCount} = counter;
+
+  // const {count, doubleCount} = storeToRefs(counter);
+  const count = computed(() => counter.count);
+  const doubleCount = computed(() => counter.doubleCount);
+
+  const { increment } = counter;
 
   console.log(props);
 </script>
