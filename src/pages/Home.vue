0b0c33de4990f08b68062ab0c21c808e67103c0b
leelanarasimha <leela.narsimha@gmail.com>
2023-06-07 09:57:29 +0530

    state methods in option api

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,11 @@
   <div>Home Page</div>
   <div>Counter: {{ count }}</div>
   <div>doubleCount: {{ doubleCount }}</div>
+  <div>Name: {{ name }}</div>
   <!-- <div>doubleValue: {{ doubleValue }}</div> -->
   <div>
     <button @click="add()">Increment</button>
+    <button @click="$reset()">Reset</button>
   </div>
 </template>
 
@@ -18,12 +20,20 @@
       //   doubleCounter: 'doubleCount',
       //   doubleValue: (state) => state.doubleCount + 1
       // }),
-      ...mapWritableState(useCounterStore, ['count', 'doubleCount'])
+      ...mapWritableState(useCounterStore, ['count', 'doubleCount', 'name'])
+    },
+    mounted() {
+      this.$subscribe((mutation, state) => {
+        console.log(mutation), console.log(state);
+      });
     },
     methods: {
-      ...mapActions(useCounterStore, ['increment']),
+      ...mapActions(useCounterStore, ['increment', '$reset', '$patch', '$subscribe']),
       add() {
-        this.count++;
+        this.$patch({
+          count: this.count + 1,
+          name: Math.random() + 'dsds'
+        });
       }
     }
   };
